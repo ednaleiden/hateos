6ee8528e64a9cd904f471411ada0d4e75818d133
ednaleiden <ednadupont@hotmail.com>
2024-07-22 09:42:39 -0500

    services configure

--- src/main/java/com/hateos/service/CuentaServices.java
+++ src/main/java/com/hateos/service/CuentaServices.java
@@ -1,9 +1,12 @@
 package com.hateos.service;
 
+import com.hateos.model.cuenta;
 import com.hateos.repository.CuentaRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional  //ejecutar una transaccion rollback o commit
@@ -11,6 +14,10 @@ public class CuentaServices {
 
     @Autowired
     private CuentaRepository cuentaRepository;
+
+    public List<cuenta> listAll(){
+        return cuentaRepository.findAll();
+    }
 
 
 
